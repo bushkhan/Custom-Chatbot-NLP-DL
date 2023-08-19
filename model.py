ac237d3687d3c19eaf8319d20a657d66ea05f95f
Bushra Khan <bushrakhan2212002@gmail.com>
2023-08-19 22:21:22 +0530

    changes

--- model.py
+++ model.py
@@ -7,7 +7,7 @@ class NeuralNet(nn.Module):
         self.l1 = nn.Linear(input_size, hidden_size)
         self.l2 = nn.Linear(hidden_size, hidden_size)
         self.h3 = nn.Linear(hidden_size, num_classes)
-        
+        self.relu = nn.ReLU
         
     def forward(self, x):
         out = self.l1(x)
--- train.py
+++ train.py
@@ -60,10 +60,35 @@ batch_size = 8
 hidden_size = 8
 output_size = len(tags)
 input_size = len(X_train[0])
-print(output_size)
+learning_rate= 0.001
+num_epochs = 1000
+
 
 dataset = ChatDataset()
 train_loader = DataLoader(dataset=dataset,batch_size=batch_size, shuffle=True, num_workers=0)
 
+device = torch.device('cupa' if torch.cuda.is_available() else 'cpu')
+model = NeuralNet(input_size, hidden_size, output_size).to(device)
 
-model = NeuralNet(input_size, hidden_size, output_size)
+#loss and optimizer
+criterion = nn.CrossEntropyLoss()
+optimizer = torch.optim.Adam(model.parameters(), lr= learning_rate)
+
+for epoch in range(num_epochs):
+    for (words, labels) in train_loader:
+        words = words.to(device)
+        labels = labels.to(device)
+        
+        #forward
+        outputs = model(words)
+        loss = criterion(outputs, labels)
+        
+        #backwardpass
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+        
+    if (epoch+1) % 100 == 0:
+        print(f'epoch {epoch+1}/{num_epochs}, loss= {loss.item():.4f}')
+    
+print(f'final loss, loss={loss.item():.4f}') 
