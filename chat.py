5ec4525cf7a87ddb894694eb15eda686b3e1aa94
Aalam <aalamansari1712@gmail.com>
2023-08-26 11:55:23 +0530

    update intents

--- chat.py
+++ chat.py
@@ -5,7 +5,7 @@ from model import NeuralNet
 from nltk_utils import bag_of_words, tokenize
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 with open('intents.json', 'r') as json_data:
-    intents = json.load(json_data)
+    intents = json.load(json_data)      
     
     
 FILE = "data.pth"
@@ -27,6 +27,7 @@ model.eval()
 bot_name = "Amy"
 print("Let's chat! type 'quit to exit")
 while True:
+    
     sentence = input('You: ')
     if sentence == 'quit':
         break
